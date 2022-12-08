bef3c0ad3618cc886cb04f8b7b740918996695dc
kayce <justinross28@gmail.com>
2022-12-08 08:22:39 -0800

    got app working and navbar on page

---
+++ react-portfolio/src/components/Greeting.js
@@ -0,0 +1,23 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function ColorSchemesExample() {
+  return (
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#Home">Justin's Portfolio</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#Home">About Me</Nav.Link>
+            <Nav.Link href="#Portfolio">Portfolio</Nav.Link>
+            <Nav.Link href="#Contact">Contact</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <br />
+    </>
+  );
+}
+
+export default ColorSchemesExample;
